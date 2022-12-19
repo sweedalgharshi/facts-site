1da2301d24a57b481b6f08091f5c0e7cffac2719
sweedalgharshi <sweedalgracius@gmail.com>
2022-12-19 16:01:33 +0530

    Creating App Component getting started

--- facts-site/src/App.js
+++ facts-site/src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import React from "react";
+import "./style.css";
+// import Logo from "./img/logo.png";
+
+const App = () => {
+  return (
+    // header section
+    <header className="header">
+      <div className="logo">
+        <img
+          src="logo.png"
+          alt="logo
+        "
+        />
+        <h1>Facts Sites!</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+};
 
 export default App;

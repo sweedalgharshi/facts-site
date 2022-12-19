cd2dfae6c76f7907113a404dd4e17efc8185fae6
sweedalgharshi <sweedalgracius@gmail.com>
2022-12-19 16:24:35 +0530

    Dividing interface into components

--- facts-site/src/App.js
+++ facts-site/src/App.js
@@ -4,20 +4,36 @@ import "./style.css";
 
 const App = () => {
   return (
-    // header section
-    <header className="header">
-      <div className="logo">
-        <img
-          src="logo.png"
-          alt="logo
-        "
-        />
-        <h1>Facts Sites!</h1>
-      </div>
+    <>
+      {/* Header Section */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="logo" />
+          <h1>Facts Sites!</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilters />
+        <FactList />
+      </main>
+    </>
   );
 };
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilters() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Fact Lists</section>;
+}
 
 export default App;

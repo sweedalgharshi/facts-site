63f2b8f6908be7d3522db2189e6bb5a37ba80567
sweedalgharshi <sweedalgracius@gmail.com>
2022-12-20 14:51:33 +0530

    Rendering the list of facts

--- facts-site/src/App.js
+++ facts-site/src/App.js
@@ -2,9 +2,53 @@ import React from "react";
 import "./style.css";
 // import Logo from "./img/logo.png";
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source: "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 const App = () => {
   return (
-    <>
+    <div>
       {/* Header Section */}
       <header className="header">
         <div className="logo">
@@ -20,7 +64,7 @@ const App = () => {
         <CategoryFilters />
         <FactList />
       </main>
-    </>
+    </div>
   );
 };
 
@@ -33,7 +77,36 @@ function CategoryFilters() {
 }
 
 function FactList() {
-  return <section>Fact Lists</section>;
+  const facts = initialFacts;
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <li className="fact" key={fact.id}>
+            <p>
+              {fact.text}
+              <a className="source" href={fact.source} target="_blank" rel="noreferrer">
+                (Source)
+              </a>
+            </p>
+            <span
+              className="tag"
+              style={{
+                backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color,
+              }}
+            >
+              {fact.category}
+            </span>
+            <div className="vote-btn">
+              <button>👍 {fact.votesInteresting}</button>
+              <button>🤯 {fact.votesMindblowing}</button>
+              <button>⛔️ {fact.votesFalse}</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default App;

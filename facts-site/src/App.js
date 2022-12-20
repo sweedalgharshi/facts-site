677aa9f8e2f773bb6b313cd42a9b215ec557cf3a
sweedalgharshi <sweedalgracius@gmail.com>
2022-12-20 20:23:24 +0530

    Adding a new fact part 1

--- facts-site/src/App.js
+++ facts-site/src/App.js
@@ -96,25 +96,46 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+// console.log("http://example.com: "+isValidHttpUrl("https://example.com"));
+// console.log("example.com: "+isValidHttpUrl("example.com"));
+
 function NewFactForm() {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("");
+  const [source, setSource] = useState("https://example.com");
   const [category, setCategory] = useState("");
   const textLength = text.length;
 
   const handleSubmit = (e) => {
     // 1. Prevent browser reload
     e.preventDefault();
+    console.log(text, source, category);
 
     // 2. Check if data is valid. If so, create a new fact
-
-    // 3. Create a new fact object
-
-    // 4. Add the new fact to the UI: add the fact to state
-
-    // 5. Reset the input fields
-
-    // 6. Close the form
+    if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      // 3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 100000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+      // 4. Add the new fact to the UI: add the fact to state
+      // 5. Reset the input fields
+      // 6. Close the form
+    }
   };
 
   return (

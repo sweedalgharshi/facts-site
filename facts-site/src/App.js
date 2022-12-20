8efbac454a14d6ee080d875da3cca9a6ecbb9d53
sweedalgharshi <sweedalgracius@gmail.com>
2022-12-20 15:12:02 +0530

    Passsing and receiving props

--- facts-site/src/App.js
+++ facts-site/src/App.js
@@ -82,30 +82,38 @@ function FactList() {
     <section>
       <ul className="facts-list">
         {facts.map((fact) => (
-          <li className="fact" key={fact.id}>
-            <p>
-              {fact.text}
-              <a className="source" href={fact.source} target="_blank" rel="noreferrer">
-                (Source)
-              </a>
-            </p>
-            <span
-              className="tag"
-              style={{
-                backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color,
-              }}
-            >
-              {fact.category}
-            </span>
-            <div className="vote-btn">
-              <button>👍 {fact.votesInteresting}</button>
-              <button>🤯 {fact.votesMindblowing}</button>
-              <button>⛔️ {fact.votesFalse}</button>
-            </div>
-          </li>
+          <Fact key={fact.id} fact={fact} />
         ))}
       </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
     </section>
+  );
+}
+
+function Fact({ fact }) {
+  console.log(fact);
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank" rel="noreferrer">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-btn">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
   );
 }
 
